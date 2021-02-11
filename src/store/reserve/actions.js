7d85966479c1343815da906ebae535caa431933a
Lemmy DongSeok Ma <silverwing@gmail.com>
2021-02-12 01:44:27 +0900

    restructuring foler

--- src/store/reserve/actions.js
+++
@@ -1,7 +0,0 @@
-export function setReservations ({commit}, payload) {
-  commit('reserve/setReservations',payload,{root:true})
-}
-
-export function setReservation({commit}, payload){
-  commit('reserve/setReservation',payload,{root:true})
-}
--- src/store/reserve/state.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-  return {
-    reserves: [],
-    startTime: '10:00',
-    endTime: '18:00'
-  }
-}
